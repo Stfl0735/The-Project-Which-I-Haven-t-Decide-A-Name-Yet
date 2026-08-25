aa7c0b78f8672dd78f906c4f1cac3ea1d44667fd
Ahmet Şataflı <asatafli235@gmail.com>
2026-08-25 03:25:34 +0300

    Modify list_creator range and update main function
    Updated the range in list_creator function and adjusted the main function to call tcp_connect_scanner with the correct parameters.

--- tcp_connect_scanner.py
+++ tcp_connect_scanner.py
@@ -20,14 +20,13 @@ def tcp_connect_scanner(hostaddr="",ports=[0],tout_val=3):
                  mysock.close()
 
                   
-def list_creator(minval=0,maxval=9999999):
-    list = [x for x in range(minval,maxval)]
-    return list
+def list_creator(minval=0,maxval=63565):
+    mylist = [x for x in range(minval,maxval)]
+    return mylist
           
             
 
 def main():
-    #tcp_connect_scanner("192.168.56.101",[21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38],4)             
-    x = list_creator(0,100000000000000)
-    print(x)  
+    tcp_connect_scanner("192.168.56.101",[21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38],4)             
+     
 main()
